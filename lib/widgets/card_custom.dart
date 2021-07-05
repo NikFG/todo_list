013893c726984bee83a056e832276503bfbe4dae
Nikollas <nikollasferreirag@gmail.com>
2021-07-05 09:31:20 -0300

    insere data formatada

--- lib/widgets/card_custom.dart
+++ lib/widgets/card_custom.dart
@@ -1,54 +1,55 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:todo_list/modelos/tarefa.dart';
-import 'package:todo_list/stores/tarefa_store.dart';
+import "package:flutter/material.dart";
+import "package:flutter_mobx/flutter_mobx.dart";
+import "package:intl/intl.dart";
+import "package:todo_list/modelos/tarefa.dart";
 
 class CardCustom extends StatelessWidget {
+  const CardCustom(
+      {required this.tarefa, required this.apaga, required this.conclui});
+
   final Tarefa tarefa;
   final VoidCallback? apaga;
   final VoidCallback? conclui;
 
-  const CardCustom({required this.tarefa,
-    required this.apaga,
-    required this.conclui});
-
   @override
   Widget build(BuildContext context) {
-    return Observer(builder: (context) =>
-        Card(
-          color: tarefa.concluido ? Color(0xFF00a86b) : Theme
-              .of(context)
-              .cardColor,
-          child: ListTile(
-              onTap: () {
-                print(tarefa.toJson());
-              },
-              title: Text(
-                tarefa.descricao,
-                style: TextStyle(decoration: TextDecoration.lineThrough),
-              ),
-              subtitle: Text(
-                "teste",
-              ),
-              trailing: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                    onPressed: conclui,
-                    icon: Icon(
-                      Icons.check,
-                      color: Colors.green,
-                    ),
+    final String formattedDate =
+        DateFormat("dd/MM/yyyy  HH:mm").format(tarefa.dataHora);
+    return Observer(
+        builder: (context) => Card(
+              color: tarefa.concluido
+                  ? Color(0xFF00a86b)
+                  : Theme.of(context).cardColor,
+              child: ListTile(
+                  onTap: () {
+                    // print(tarefa.toJson());
+                  },
+                  title: Text(
+                    tarefa.descricao,
+                    style: TextStyle(),
                   ),
-                  IconButton(
-                    onPressed: apaga,
-                    icon: Icon(
-                      Icons.delete,
-                      color: Colors.red,
-                    ),
+                  subtitle: Text(
+                    formattedDate,
                   ),
-                ],
-              )),
-        ));
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        onPressed: conclui,
+                        icon: Icon(
+                          Icons.check,
+                          color: Colors.green,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: apaga,
+                        icon: Icon(
+                          Icons.delete,
+                          color: Colors.red,
+                        ),
+                      ),
+                    ],
+                  )),
+            ));
   }
 }
